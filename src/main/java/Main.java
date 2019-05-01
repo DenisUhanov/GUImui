0c430d31c73d18d621fe771069cc346132fad299
Denis <denis.hanov@gmail.com>
2019-05-01 10:50:38 +0300

    Подготовка

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import com.mysql.jdbc.Connection;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] argv) {
+
+         }
+}
+
