d33835d0d02f5d47a8cb9860b1d67d5c9b4ca259
Denis <denis.hanov@gmail.com>
2019-05-03 21:43:46 +0300

    add JFrame

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,13 @@
-import com.mysql.jdbc.Connection;
-
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] argv) {
+        JFrame jFrame = new JFrame();
+        jFrame.setVisible(true);
+        jFrame.setBounds(750,250,700,200);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-         }
+        jFrame.revalidate();
+    }
 }
 
